f815a4be78c2584e5923b3b2a75ff43a8837c23d
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-09-11 11:16:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Inicijalno_ponavljanje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upiši te URL adresu: ");
+            string url = Console.ReadLine();
+            if(url.StartsWith("www.") && url.EndsWith(".hr"))
+            {
+                Console.WriteLine("URL adresa je u ispravnom formatu");
+            }
+            else
+            {
+                Console.WriteLine("URL adresa nije u ispravnom formatu");
+            }
+            Console.ReadKey();
+        }
+    }
+}
